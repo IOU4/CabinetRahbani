3473d90716ca9ae199de17d87befd652ab041fa3
Redadaalabi2001 <daalabireda@gmail.com>
2022-01-25 16:05:47 +0100

    restructure the project filesystem

--- web_site/index.php
+++ web_site/index.php
@@ -4,26 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion de Cabine</title>
-    <link rel="shortcut icon" href="./Images/Icon.ico"/>
-    <!-- link css file  -->
-    <link rel="stylesheet" href="CSS/index.css">
-    <link rel="stylesheet" href="CSS/login.css">
-    <!-- link fint awesome library -->
-    <link rel="stylesheet" href="CSS/all.min.css">
-    <!-- render all elements noemally -->
-    <link rel="stylesheet" href="CSS/normalise.css"> 
-    <!-- link google font -->
+    <title>Gestion de Cabinet</title>
+    <link rel="shortcut icon" href="assets/ico.ico"/>
+    <link rel="stylesheet" href="styles/home.css">
+    <link rel="stylesheet" href="styles/login.css">
+    <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/normalise.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200;300;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <!-- start header -->
-    <div class="header">
+    <!-- <div class="header">
         <div class="container">
-            <img src="./Images/Logo.png" alt="" class="logo">    
-            <a class="login-1" href="./login-Admin.html">Login</a>
+            <img src="assets/Logo.png" alt="" class="logo">    
+            <a class="login-1" href="./login.php">Login</a>
             <div class="icons">
                 <i class="fas fa-envelope"></i>
                 <span>rahbanirafik@gmail.com</span>
@@ -31,27 +29,16 @@
                 <span class="seconde-icon">+212 561-753209</span>
             </div>
         </div>
-    </div>
+    </div> -->
+    <?php include './components/header.php';?>
     <!-- start header -->
-    <!-- start content -->
-    <div class="main">
-        <p class="login">Login</p>
-        <ul class="main-nav">
-            <a class="hove" href="./login-Admin.html">Admin</a>
-            <a class="bold hove" href="./login-Médecin.html">Médecin</a>
-            <a class="hove" href="./login-Patient.html">Patient</a>
-        </ul>
-        <form class="form">
-          <input class="un " type="text"placeholder="E-mail">
-          <input class="pass" type="password" placeholder="Password">
-          <a class="submit" href="#" >Login</a>    
-          <a class="forgot" href="#">Forgot Password ?</a> 
-    </div>
+    <!-- start content -->   
+    <?php include './components/home.php';?>
     <!-- end content -->
     <!-- start footer -->
-    <div class="footer">
+    <!-- <div class="footer">
         <div class="container">
-            <img src="./Images/Logo.png" class="logo" alt="">
+            <img src="assets/Logo.png" class="logo" alt="">
             <p>WE ARE SOCIAL</p>
             <div class="social-icons">
                 <i class="fas fa-globe"></i>
@@ -65,7 +52,8 @@
                 &copy; 2022 - <span>Cabinet Rahbani</span> All right Reserved
             </p>
         </div>
-    </div>
+    </div> -->
+    <?php include'./components/footer.php'; ?>
     <!-- end footer -->
 </body>
 </html>
---
+++ web_site/src/components/header.php
@@ -0,0 +1,13 @@
+<header class="header">
+  <div class="logo-container"><img src="./assets/Logo.png" alt="cabinet rahbani logo"></div>
+  <div class="contact-info">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path d="M10.554 6.24L7.171 2.335c-.39-.45-1.105-.448-1.558.006L2.831 5.128c-.828.829-1.065 2.06-.586 3.047a29.207 29.207 0 0 0 13.561 13.58c.986.479 2.216.242 3.044-.587l2.808-2.813c.455-.455.456-1.174.002-1.564l-3.92-3.365c-.41-.352-1.047-.306-1.458.106l-1.364 1.366a.462.462 0 0 1-.553.088a14.557 14.557 0 0 1-5.36-5.367a.463.463 0 0 1 .088-.554l1.36-1.361c.412-.414.457-1.054.101-1.465z" stroke="#5ae4a8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g></svg>
+      <span>+21258899898</span>
+    </div>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6z" stroke="#5ae4a8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M2 8l7.501 6.001a4 4 0 0 0 4.998 0L22 8" stroke="#5ae4a8" stroke-width="2" stroke-linejoin="round"/></g></svg>
+      <span>contac@cabinerrahbani.com</span>
+    </div>
+  </div>
+</header>
