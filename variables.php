3a0be0bffa570a4dca9a6be8335dba36ca162e40
Emadou <ouchaibimad000@gmail.com>
2022-03-08 23:09:54 +0100

    heroku

---
+++ variables.php
@@ -0,0 +1,5 @@
+<?php
+define('ASSETS', $_SERVER['DOCUMENT_ROOT']."/web_site/src/assets");
+define('COMPONENTS', $_SERVER['DOCUMENT_ROOT']."/web_site/src/components");
+define('STYLES', $_SERVER['DOCUMENT_ROOT']."/web_site/src/styles");
+define('DB', $_SERVER['DOCUMENT_ROOT']."/web_site/database");
